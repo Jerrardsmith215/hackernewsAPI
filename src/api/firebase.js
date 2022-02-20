51b2d21ceae46e07a17444b331660ff3f7a292b6
Jerrard Smith <jerrardsmith215@gmail.com>
2022-02-20 00:05:15 -0500

    firebase successfully initialized

---
+++ src/api/firebase.js
@@ -0,0 +1,17 @@
+// configure firebase with database product
+import { initializeApp } from "firebase/app";
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4kNEysuj6aJw-icnD4jq8-OlXRbiHwbk",
+  authDomain: "hackernewstest-68bf6.firebaseapp.com",
+  projectId: "hackernewstest-68bf6",
+  storageBucket: "hackernewstest-68bf6.appspot.com",
+  messagingSenderId: "333000170340",
+  appId: "1:333000170340:web:67e011ff895a17c2d43871",
+  databaseURL: "https://hacker-news.firebaseio.com/"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+export default db;
